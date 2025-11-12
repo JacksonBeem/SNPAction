c16240282eb321f09db35224649fa1a45104f552
JacksonBeem <156236541+JacksonBeem@users.noreply.github.com>
2025-11-12 15:53:11 -0500

    Refactor variables in variables.tf
    Refactor variable definitions for better readability.

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,2 +1,9 @@
-variable "project_name" { type = string  default = "SNP" }
-variable "aws_region"   { type = string  default = "us-east-2" }
+variable "project_name" {
+  type    = string
+  default = "SNP"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-2"
+}
